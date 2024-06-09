b0c29a78d83c16cfcb63bfddbedc25f1ba9a34c4
bebr2 <1215427670@qq.com>
2024-06-09 16:39:13 +0800

    gg

--- src/components/SubPage_PDFs.vue
+++ src/components/SubPage_PDFs.vue
@@ -335,7 +335,9 @@ export default {
         };
     };
     const rowCreateTime = ({ row }) => {
-      let date = new Date(row.created_time);
+      let date = new Date(row.created_time*1000);
+      // console.log(row.created_time);
+      // console.log(date);
       const year = date.getFullYear();
       const month = String(date.getMonth() + 1).padStart(2, "0");
       const day = String(date.getDate()).padStart(2, "0");
